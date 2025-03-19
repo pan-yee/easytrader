e7ab2cc175218cfd23557e1aacc8666c1943e61a
yp <yipan@yto.net.cn>
2025-03-19 14:12:11 +0800

    主函数

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import easytrader
 
 
-def main():
+def xq():
     user = easytrader.use('xq')
     user.prepare('xq.json')
     # print("获取资金状况:" + user.balance)
@@ -11,6 +11,11 @@ def main():
     xq_follower = easytrader.follower('xq')
     xq_follower.login(cookies=user.account_config.get("cookies"))
     xq_follower.follow(user, user.account_config.get("portfolio_code"))
+def yh():
+    user = easytrader.use('yh')
+
+def main():
+    xq()
 
 if __name__ == '__main__':
     main()
