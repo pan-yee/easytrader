0c31a5feba064eac0ad6b068f016891903ed5001
yp <yipan@yto.net.cn>
2025-04-03 17:15:29 +0800

    优化

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import easytrader
 
 
 def xq():
+    # 创建雪球用户并登陆
     user = easytrader.use('xq')
     user.prepare('xq.json')
     # print("获取资金状况:" + user.balance)
@@ -10,12 +11,26 @@ def xq():
     print(user.account_config.get("cookies"))
     xq_follower = easytrader.follower('xq')
     xq_follower.login(cookies=user.account_config.get("cookies"))
-    xq_follower.follow(user, user.account_config.get("portfolio_code"))
+    # 创建银河用户并登陆
+    userYh = yh()
+    xq_follower.follow(userYh,
+                       user.account_config.get("portfolio_code"),
+                       100000,
+                       None,
+                       False,
+                       1,
+                       1200000000,
+                       True,
+                       0.01)
+
+
 def yh():
-    user = easytrader.use('yh')
+    return easytrader.use('yh')
+
 
 def main():
     xq()
+
 
 if __name__ == '__main__':
     main()
