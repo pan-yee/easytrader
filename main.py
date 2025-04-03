57fc9972bd71d96e3fd359b0ad5ba45518d6efe1
yp <yipan@yto.net.cn>
2025-04-03 19:31:42 +0800

    支持同花顺

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from http.cookiejar import debug
-
 import easytrader
 
 
-def xq():
+def xqToYh():
     # 创建雪球用户并登陆
     user = easytrader.use('xq')
     user.prepare('xq.json')
@@ -25,16 +23,41 @@ def xq():
                        cmd_cache=True,
                        slippage=0.01, )
 
-
+def xqToThs():
+    # 创建雪球用户并登陆
+    user = easytrader.use('xq')
+    user.prepare('xq.json')
+    # print("获取资金状况:" + user.balance)
+    # print("获取持仓:" + user.position)
+    print(user.account_config)
+    print(user.account_config.get("cookies"))
+    xq_follower = easytrader.follower('xq')
+    xq_follower.login(cookies=user.account_config.get("cookies"))
+    # 创建同花顺通用用户并登陆
+    userThs = ths()
+    xq_follower.follow(userThs,
+                       user.account_config.get("portfolio_code"),
+                       total_assets=100000,
+                       initial_assets=None,
+                       adjust_sell=True,
+                       track_interval=1,
+                       trade_cmd_expire_seconds=120000000,
+                       cmd_cache=True,
+                       slippage=0.01, )
 def yh():
-    userYh = easytrader.use('yh_client',debug)
+    userYh = easytrader.use('yh_client',debug=True)
     userYh.prepare('yh_client.json')
     userYh.login()
     return userYh
+def ths():
+    userThs = easytrader.use('universal_client',debug=True)
+    userThs.prepare(user='用户名', password='雪球、银河客户端为明文密码', exe_path='E:\\同花顺软件\\同花顺\\xiadan.exe', comm_password='华泰通讯密码，其他券商不用')
+    userThs.login(user='用户名', password='雪球、银河客户端为明文密码', exe_path='E:\\同花顺软件\\同花顺\\xiadan.exe', comm_password='华泰通讯密码，其他券商不用')
+    return userThs
 
 
 def main():
-    xq()
+    xqToThs()
 
 
 if __name__ == '__main__':
